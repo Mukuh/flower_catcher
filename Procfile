e2003ed655ed0bc832d0b14d6777180adc47481b
Mukuh <tstelz@web.de>
2021-09-15 20:57:39 +0200

    [code] try to start heroku app via the “application factory” pattern

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:app
+web: gunicorn 'flowr:create_app()'
--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flowr import create_app
 
-app = create_app()
-app.run(debug=True)
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True)
 
